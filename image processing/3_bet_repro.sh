7c893264264900ecb8063f01345dbb2394dacdb4
goschv <119061349+goschv@users.noreply.github.com>
2023-01-09 15:25:50 +0100

    Update 3_bet_repro.sh

--- image processing/3_bet_repro.sh
+++ image processing/3_bet_repro.sh
@@ -23,3 +23,10 @@ bet $s/*b0.nii.gz  $s/processed/"${s:9:11}"_b0_bet.nii.gz -f 0.2 -R
 fast -n 2 -I 1 -l 20 -t 2 -o $s/processed/"${s:9:11}"_fast $s/processed/*b0_bet.nii.gz   
 
 done 
+
+
+# references
+
+'https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/FSL
+ 32 Woolrich MW, Jbabdi S, Patenaude B, Chappell M, Makni S, Behrens T, et al. Bayesian analysis of neuroimaging data in FSL. Neuroimage. 2009;45: S173–86.
+ 27 Zhang Y, Brady M, Smith S. Segmentation of brain MR images through a hidden Markov random field model and the expectation-maximization algorithm. IEEE Trans Med Imaging. 2001;20: 45–57.'
