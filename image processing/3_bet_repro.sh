de3aca9cee22a9c4499166f9dea4b72ba4a23802
goschv <119061349+goschv@users.noreply.github.com>
2023-01-09 15:33:03 +0100

    Update 3_bet_repro.sh

--- image processing/3_bet_repro.sh
+++ image processing/3_bet_repro.sh
@@ -28,5 +28,6 @@ done
 # references
 
 'https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/FSL
- 32 Woolrich MW, Jbabdi S, Patenaude B, Chappell M, Makni S, Behrens T, et al. Bayesian analysis of neuroimaging data in FSL. Neuroimage. 2009;45: S173–86.
- 27 Zhang Y, Brady M, Smith S. Segmentation of brain MR images through a hidden Markov random field model and the expectation-maximization algorithm. IEEE Trans Med Imaging. 2001;20: 45–57.'
+32 Woolrich MW, Jbabdi S, Patenaude B, Chappell M, Makni S, Behrens T, et al. Bayesian analysis of neuroimaging data in FSL. Neuroimage. 2009;45: S173–86.
+21	Smith SM. Fast robust automated brain extraction. Hum Brain Mapp. 2002;17: 143–155.
+27 Zhang Y, Brady M, Smith S. Segmentation of brain MR images through a hidden Markov random field model and the expectation-maximization algorithm. IEEE Trans Med Imaging. 2001;20: 45–57.'
