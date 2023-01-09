4e5d56e26801e6f08397427330a54fe2cd365311
goschv <119061349+goschv@users.noreply.github.com>
2023-01-09 15:42:05 +0100

    Update 5_registration_repro.sh

--- image processing/5_registration_repro.sh
+++ image processing/5_registration_repro.sh
@@ -55,3 +55,14 @@ cat $s/processed/ROI_mas_quickTRACE.txt
 done 
 
 done 
+
+# references
+
+'https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/FSL
+32 Woolrich MW, Jbabdi S, Patenaude B, Chappell M, Makni S, Behrens T, et al. Bayesian analysis of neuroimaging data in FSL. Neuroimage. 2009;45: S173–86.
+22 Avants BB, Tustison NJ, Song G, Cook PA, Klein A, Gee JC. A reproducible evaluation of ANTs similarity metric performance in brain image registration. Neuroimage. 2011;54: 2033–2044.
+23 Avants BB, Epstein CL, Grossman M, Gee JC. Symmetric diffeomorphic image registration with cross-correlation: evaluating automated labeling of elderly and neurodegenerative brain. Med Image Anal. 2008;12: 26–41.
+24 Mazziotta J, Toga A, Evans A, Fox P, Lancaster J, Zilles K, et al. A four-dimensional probabilistic atlas of the human brain. J Am Med Inform Assoc. 2001;8: 401–430.
+25 Mazziotta J, Toga A, Evans A, Fox P, Lancaster J, Zilles K, et al. A probabilistic atlas and reference system for the human brain: International Consortium for Brain Mapping (ICBM). Philos Trans R Soc Lond B Biol Sci. 2001;356: 1293–1322.
+26 Mazziotta JC, Toga AW, Evans A, Fox P, Lancaster J. A probabilistic atlas of the human brain: Theory and rationale for its development. Neuroimage. 1995;2: 89–101.
+'
