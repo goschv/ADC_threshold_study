1418c6a9e4803353167cc92882a60456dbc8b66e
goschv <119061349+goschv@users.noreply.github.com>
2023-01-09 15:58:08 +0100

    Update 3_bet_repro.sh

--- image processing/3_bet_repro.sh
+++ image processing/3_bet_repro.sh
@@ -18,9 +18,7 @@ echo $s
 
 bet $s/*b0.nii.gz  $s/processed/"${s:9:11}"_b0_bet.nii.gz -f 0.2 -R
 
-
-# fast extract images
-fast -n 2 -I 1 -l 20 -t 2 -o $s/processed/"${s:9:11}"_fast $s/processed/*b0_bet.nii.gz   
+ 
 
 done 
 
@@ -29,5 +27,4 @@ done
 
 'https://fsl.fmrib.ox.ac.uk/fsl/fslwiki/FSL
 32 Woolrich MW, Jbabdi S, Patenaude B, Chappell M, Makni S, Behrens T, et al. Bayesian analysis of neuroimaging data in FSL. Neuroimage. 2009;45: S173–86.
-21	Smith SM. Fast robust automated brain extraction. Hum Brain Mapp. 2002;17: 143–155.
 27 Zhang Y, Brady M, Smith S. Segmentation of brain MR images through a hidden Markov random field model and the expectation-maximization algorithm. IEEE Trans Med Imaging. 2001;20: 45–57.'
